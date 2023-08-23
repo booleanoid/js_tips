bf9ea5f2cff95ae715f6c90336dedb795292e46d
booleanoid <booleanoid@gmail.com>
2023-08-24 01:23:59 +0900

    add obj tips

---
+++ tips/class.js
@@ -0,0 +1,10 @@
+﻿const obj = {
+    kinds: "stone",
+    status: "active",
+    color: ["red"],
+    weight: 1500,
+}
+console.log(obj.kinds);
+obj.color.push("blue");
+console.log(obj.color);
+console.log(obj.color.length);
